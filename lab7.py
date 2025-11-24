daf0e57dc03e6de0c2f746a15ea0c1f82e7e5e45
sumereget <sumerege.tunca@std.yeditepe.edu.tr>
2025-11-24 17:02:09 +0300

    Add files via upload

--- lab7.py
+++ lab7.py
@@ -1,11 +1,9 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def centroid(p1x, p1y, p2x, p2y, p3x, p3y, m1, m2, m3):
-    
-    
     positions = np.array([[p1x, p2x, p3x],
-                          [p2x, p2y, p3y]]) #2D
+                          [p2y, p2y, p3y]]) #2D
     
     masses = np.array([m1, m2, m3]) #1D
     
